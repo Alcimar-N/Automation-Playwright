68edf17535ee92a7b0b91e15ed397f89e3ea6a15
user.e-mail <alcimar_nf@hotmail.com>
2023-03-27 21:12:59 -0300

    atualizado

--- test/login.spec.ts
+++ test/login.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 import { Classe } from '../pages/pageObject'; // importei a classe com nome "Classe" criada na pasta "pageObjects"
 
 // aqui escrevo os testes chamando os metodos criados na classe da pagina pageObjects
-
+// teste 
 test.describe('Login', () => {
 
 
