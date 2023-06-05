ff388e5b3c0d0dc96398afe838c26268269fb892
user.e-mail <alcimar_nf@hotmail.com>
2023-06-05 20:08:41 -0300

    finaliza compra

--- pages/carrinhoPage.ts
+++ pages/carrinhoPage.ts
@@ -12,21 +12,24 @@ export class carrinhoClasse {
   async AddProduto() {
     //await this.page.locator('[id=add-to-cart-sauce-labs-backpack]',).click()
     // elementos dentro das variaveis element1, 2
-    const element1 = await this.page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]')
-    const element2 = await this.page.waitForSelector('#add-to-cart-sauce-labs-bolt-t-shirt')
+    const element1 = await this.page.locator('#add-to-cart-sauce-labs-backpack')
+    const element2 = await this.page.waitForSelector('[data-test="add-to-cart-sauce-labs-bike-light"]')
+    const element3 = await this.page.waitForSelector('#add-to-cart-sauce-labs-bolt-t-shirt')
    
     await Promise.all([ // promisse.all() aguarda que várias açoes sejam concluida para seguir
       element1.click(),
-      element2.click()
+      element2.click(),
+      element3.click()
+      
     ])
     await this.AcessaCarrinho()
-    await this.page.waitForTimeout(5000)
+  
   }
 
   async AcessaCarrinho() {
     await this.page.waitForTimeout(2000)
     await this.page.locator('[id^="shopping"]').click()
-    await this.page.waitForTimeout(8000)
+    
 
   }
 
@@ -37,12 +40,18 @@ export class carrinhoClasse {
   }
 
   async FinalizaCompra() {
-    await this.AcessaCarrinho()
     await this.AddProduto()
-  
+    await this.AcessaCarrinho()
+    await this.page.locator('#checkout').click()
+    await this.page.locator('[placeholder="First Name"]').fill('Teste')
+    await this.page.locator('[placeholder="Last Name"]').fill('Testando')
+    await this.page.locator('#postal-code').fill('03370020')
+    await this.page.locator('#continue').click()
+    await this.page.locator('#finish').click()
+    await this.page.waitForTimeout(3000)
   }
 
-  async NovoTeste(){
+  async Checkout(){
     
   }
 
