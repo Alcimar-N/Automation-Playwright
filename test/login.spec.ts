12f1166b5b349607617a2bde6ee1bb6c9181504d
user.e-mail <alcimar_nf@hotmail.com>
2023-08-25 21:53:09 -0300

    Atualização do projeto

--- test/login.spec.ts
+++ test/login.spec.ts
@@ -1,18 +1,18 @@
 import { test, expect } from '@playwright/test';
 //import { Classe } from '../pages/metodos'; // importei a classe com nome "Classe" criada na pasta "pageObjects"
 import { homePage } from '../pages/homePage';
-import { loginPage } from '../pages/loginPage';
+import { LoginPage } from '../pages/loginPage';
 // aqui escrevo os testes chamando os metodos criados na classe da pagina metodos
 
 test.describe('Login', () => {
     //let umaClasse: Classe
     let homesPage: homePage
-    let login: loginPage
+    let loginPage: LoginPage
     test.beforeEach(async ({ page }) => {
         // Roda antes da cada teste
        // umaClasse = new Classe(page)
         homesPage =new homePage(page)
-        login = new loginPage(page)
+        loginPage = new LoginPage(page)
         await page.goto('https://www.saucedemo.com/')
 
     });
@@ -20,7 +20,8 @@ test.describe('Login', () => {
         await page.close()
     });
 
-    test('home teste botão login', async() => {
-        await login.VerificaBtnLogin()
+    test('logar no sistema', async() => {
+        await loginPage.Login()
+        
     })
 })
